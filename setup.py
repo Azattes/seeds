d85eab55c879e1231883cca25c1b33c7eace7b63
Болотбеков Азат Болотбекович <abolotbekov@megacom.local>
2023-03-14 09:48:20 +0600

    feat: Added setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="seeds",
+    packages=["seeds"],
+    description="",
+    version="0.1",
+    url="https://github.com/Azattes/seeds.git",
+    author="Azat",
+    author_email="azat.kg.10@gmail.com",
+)
